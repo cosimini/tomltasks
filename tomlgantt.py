7dca35c3998a10938fc5ec420a6652d9dbf5ce1f
cosimini <davide.scalcon@studenti.unipd.it>
2024-07-03 20:56:26 +0000

    Fixing some details

--- tomlgantt.py
+++ tomlgantt.py
@@ -6,15 +6,18 @@ import toml
 
 
 class tomltasks:
-    tasks_list_features = {"title": None, "start_day": None, "deadline": None, "notes": None, "shortnames": None}
+    tasks_list_features = {"title": str, "start_day": None, "deadline": None, "notes": str, "shortnames": dict}
+    task_features = {}
+    groups_features = {"g_name": str}
 
     def __init__(self, filename):
         self.tasks = self.generate_times(self.parse_dict(toml.load(filename), []))
 
     def parse_dict(dic: dict, category: list) -> list:
+        level_dict = []
         for k in dic.keys():
             pass
-        return []
+        return level_dict
 
     def generate_times(tasks_list: list) -> list:
         pass
